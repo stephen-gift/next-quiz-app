63e2e92a29d9eb0e4f2d16b761afabc1ac8cbc9b
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-02-20 23:51:13 +0100

    update

--- app/quiz/page.tsx
+++ app/quiz/page.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import { useSearchParams } from "next/navigation";
+import { Button } from "@/components/ui/button";
 
 type Props = {};
 
@@ -11,10 +12,36 @@ const page = (props: Props) => {
   const title = searchParams.get("title");
   const quiz = searchParams.get("quiz");
   return (
-    <main>
-      <div></div>
-      <div></div>Here's the title: {title}
-      {quiz}
+    <main className="flex min-h-screen flex-row items-center justify-between p-24">
+      <div className="flex-1">
+        <p>Question 1 / 10</p>
+        <h1>Which CSS property controls the text size?</h1>
+      </div>
+      <div className="flex-1 ">
+        <ul className="">
+          <li className="mb-5">
+            <Button className="container flex justify-start items-center gap-3">
+              1 <p> text-size</p>
+            </Button>
+          </li>
+          <li className="mb-5">
+            <Button className="container flex justify-start items-center gap-3">
+              2 <p> text-size</p>
+            </Button>
+          </li>
+          <li className="mb-5">
+            <Button className="container flex justify-start items-center gap-3">
+              3 <p> text-size</p>
+            </Button>
+          </li>
+          <li className="mb-5">
+            <Button className="container flex justify-start items-center gap-3">
+              4 <p> text-size</p>
+            </Button>
+          </li>
+        </ul>
+        <Button className="w-full">Submit</Button>
+      </div>
     </main>
   );
 };
